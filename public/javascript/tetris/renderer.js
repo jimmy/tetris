c5c5e3bcfa2ff1bbc0e9ebd56ba3d72458883943
Jimmy Kittiyachavalit <jimmy@codetry.com>
2010-04-13 23:16:11 -0700

    Remove unused code.

--- public/javascript/tetris/renderer.js
+++ public/javascript/tetris/renderer.js
@@ -26,25 +26,6 @@ tetris.renderer = function(drawingCanvas) {
     }
   }
 
-  var render_piece = function(piece) {
-    var boxes = piece.get_boxes();
-    var x;
-    var y;
-
-    context.fillStyle   = '#ff0';
-
-    for (var i = 0; i < boxes.length; i += 1) {
-      x = boxes[i][0];
-      y = boxes[i][1];
-      context.fillRect(
-        x_offset + t(piece.x + x),
-        y_offset + t(piece.y + y),
-        t(1),
-        t(1)
-      );
-    }
-  };
-
   var render_tetromino = function(tetromino) {
     render_blocks(tetromino.blocks());
   };
@@ -64,7 +45,6 @@ tetris.renderer = function(drawingCanvas) {
 
   return {
     render_stage: render_stage,
-    render_piece: render_piece,
     render_blocks: render_blocks,
     render_tetromino: render_tetromino
   };
